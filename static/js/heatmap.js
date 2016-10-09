1d930b99ba6b82e0056fd50876623595a82d5457
Tristan Ray <tristanray@Tristans-MacBook-Pro.local>
2016-10-09 05:17:06 -0400

    Time buttons complete

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -2,7 +2,17 @@
  * Created by karenx on 10/9/16.
  */
 
+var currentHeatLayer = null;
+
 function loadTrafficHeatMap(map) {
+    loadTrafficHeatMap(map, 1);
+}
+
+function loadTrafficHeatMap(map, timeId) {
+    if (currentHeatLayer != null) {
+        map.removeLayer(currentHeatLayer);
+    }
+
     $.ajax({
         url: "/api/lot/nearby",
         success: function (data, result) {
@@ -23,7 +33,24 @@ function loadTrafficHeatMap(map) {
                     llr.push(lat);
                     var lng = collegeVenues[venueIndex]["longitude"];
                     llr.push(lng);
-                    var rad = collegeVenues[venueIndex]["check_ins"];
+                    var rad = 0;
+
+                    switch (timeId) {
+                        case 0:
+                            rad = collegeVenues[venueIndex]["check_ins"];
+                            break;
+                        case 1:
+                            rad = collegeVenues[venueIndex]["check_ins"] / 2;
+                            break;
+                        case 2:
+                            rad = collegeVenues[venueIndex]["check_ins"] / 2;
+                            break;
+                        case 3:
+                            rad = 0;
+                            break;
+                    }
+
+
                     llr.push(rad);
 
                     heatIndices.push(llr);
@@ -36,7 +63,23 @@ function loadTrafficHeatMap(map) {
                     llr.push(lat);
                     var lng = workVenues[venueIndex]["longitude"];
                     llr.push(lng);
-                    var rad = workVenues[venueIndex]["check_ins"];
+                    var rad = 0;
+
+                    switch (timeId) {
+                        case 0:
+                            rad = workVenues[venueIndex]["check_ins"];
+                            break;
+                        case 1:
+                            rad = workVenues[venueIndex]["check_ins"];
+                            break;
+                        case 2:
+                            rad = workVenues[venueIndex]["check_ins"] / 2;
+                            break;
+                        case 3:
+                            rad = 0;
+                            break;
+                    }
+
                     llr.push(rad);
 
                     heatIndices.push(llr);
@@ -49,7 +92,23 @@ function loadTrafficHeatMap(map) {
                     llr.push(lat);
                     var lng = nightlife[venueIndex]["longitude"];
                     llr.push(lng);
-                    var rad = nightlife[venueIndex]["check_ins"];
+                    var rad = 0;
+
+                     switch (timeId) {
+                        case 0:
+                            rad = 0;
+                            break;
+                        case 1:
+                            rad = 0;
+                            break;
+                        case 2:
+                            rad = nightlife[venueIndex]["check_ins"] / 5;
+                            break;
+                        case 3:
+                            rad = nightlife[venueIndex]["check_ins"];
+                            break;
+                    }
+
                     llr.push(rad);
 
                     heatIndices.push(llr);
@@ -62,7 +121,23 @@ function loadTrafficHeatMap(map) {
                     llr.push(lat);
                     var lng = foodLife[venueIndex]["longitude"];
                     llr.push(lng);
-                    var rad = foodLife[venueIndex]["check_ins"];
+                    var rad = 0;
+
+                    switch (timeId) {
+                        case 0:
+                            rad = foodLife[venueIndex]["check_ins"];
+                            break;
+                        case 1:
+                            rad = foodLife[venueIndex]["check_ins"];
+                            break;
+                        case 2:
+                            rad = foodLife[venueIndex]["check_ins"];
+                            break;
+                        case 3:
+                            rad = foodLife[venueIndex]["check_ins"] / 2;
+                            break;
+                    }
+
                     llr.push(rad);
 
                     heatIndices.push(llr);
@@ -70,7 +145,7 @@ function loadTrafficHeatMap(map) {
                 }
             }
 
-            L.heatLayer(heatIndices, {radius: 15}).addTo(map);
+            currentHeatLayer = L.heatLayer(heatIndices, {radius: 15}).addTo(map);
         }
 
     });
